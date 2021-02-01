c0d8023bb3b5e6077cb1abf2bdb6dfda2382c9c7
Sakai Takuro <sakait@am1>
2021-02-01 18:28:06 +0900

    modify dein install script

--- Windows/Vim/dein_install.ps1
+++ Windows/Vim/dein_install.ps1
@@ -1,5 +1,6 @@
 Invoke-WebRequest https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.ps1 -OutFile installer.ps1
 # Allow to run third-party script
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-# For example, we just use `~/.cache/dein` as installation directory
-./installer.ps1 ~/.cache/dein
+$INSTALLPREFIX = Convert-Path .
+# For example, we just use `$env:VIMHOME\cache\dein` as installation directory
+.\installer.ps1 $INSTALLPREFIX\cache\dein
