639caa78ea7254cb29fb46c1986317d93687f69f
taqu <taqu2920.dev@gmail.com>
2023-02-08 12:16:36 +0900

    Update dein_install.sh
    Fix the dein installer path

--- Linux/Vim/dein_install.sh
+++ Linux/Vim/dein_install.sh
@@ -3,6 +3,6 @@ mkdir -p ~/.cache
 cp -rf ./vim ~/.vim
 cp -rf ./cache/* ~/.cache
 cp -rf ./vimrc ~/.vimrc
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+curl -fsSL https://raw.githubusercontent.com/Shougo/dein-installer.vim/master/installer.sh > installer.sh
 sh ./installer.sh ~/.cache/dein
 
