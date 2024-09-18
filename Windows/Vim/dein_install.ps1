02049ffe5e988a2112a8c1b384baf0574e5ded80
taqu <taqu2920.dev@gmail.com>
2024-09-19 05:15:29 +0900

    Change the plugin manager dein to vim-plug

--- Windows/Vim/dein_install.ps1
+++
@@ -1,6 +0,0 @@
-Invoke-WebRequest https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.ps1 -OutFile installer.ps1
-# Allow to run third-party script
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-$INSTALLPREFIX = Convert-Path .
-# For example, we just use `$env:VIMHOME\cache\dein` as installation directory
-.\installer.ps1 $INSTALLPREFIX\cache\dein
--- Windows/Vim/templates/cpp/snip-noncopyable.cpp
+++
@@ -1,5 +0,0 @@
-{{_input_:name}}(const {{_input_:name}}&) = delete;
-{{_input_:name}}({{_input_:name}}&&) = delete;
-{{_input_:name}}& operator=(const {{_input_:name}}&) = delete;
-{{_input_:name}}& operator=({{_input_:name}}&&) = delete;
-
